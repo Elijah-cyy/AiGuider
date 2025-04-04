511bfbe6f4c97e2267636de3d4f1d3c686de9b01
Cyrus Chen <chen_yu_yang@126.com>
2025-04-04 17:25:37 +0800

    chore: 更新后端安装脚本以使用清华镜像源

--- setup_backend.sh
+++ setup_backend.sh
@@ -6,8 +6,9 @@
 cd aiGuider_Server
 
 # 安装后端依赖
-echo "安装后端依赖..."
+echo "安装项目所需依赖..."
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install -r requirements.txt
 
 # 提示完成
-echo "后端环境安装完成"
+echo "环境依赖安装完成"
